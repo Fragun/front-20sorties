9a9d60fb3ac9cc268a283527ecab4607656a7b2b
David <dethoordavid@yahoo.fr>
2023-12-19 00:22:22 +0100

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     "/api/*",
     createProxyMiddleware({
       target: "https://back-20sorties-ittp3j6dy-20-sorties.vercel.app/",
-      secure: false,
+      //secure: false,
     })
   );
 };
