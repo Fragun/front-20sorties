312e32d083535ef4a8689d272bbf650fa18127a7
David <dethoordavid@yahoo.fr>
2023-12-19 15:16:23 +0100

    debug

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-// Bootstrap CSS
+import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle";
 import './assets/styles/index.scss'; 
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-// import { GoogleOAuthProvider } from "@react-oauth/google";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-        <RouterProvider router={router}>
-      </RouterProvider>
-  </React.StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
